c4c2bf2e8a65158d7c3e02b477056d6485706d05
luisangeluis <luisangeluis@gmail.com>
2022-07-01 14:25:32 -0500

    Configurando paginacion

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,28 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const Pagination = ({ arrayPages,currentPage,setCurrentPage,quantityPages }) => {
+const Pagination = ({ arrayPages }) => {
+  console.log(arrayPages);
+  return (
+    <section className="row pagination">
+      <div className="col-12 d-flex justify-content-center w-50 mx-auto align-items-center">
+        <div>&#60;</div>
+        <ul className="d-flex justify-content-between align-items-center  m-0 w-50">
+          {arrayPages?.map((num) => (
+            <li key={num}>{num}</li>
+          ))}
+        </ul>
+        <div>&#62;</div>
+      </div>
+    </section>
+  );
+};
 
-    const prevPage=()=>{
-        if(currentPage-1==0){
-            setCurrentPage(quantityPages)
-        }else{
-            setCurrentPage(currentPage-1);
-
-        }
-    }
-    return (
-        <section className="row pagination">
-            <div className="col-12 d-flex justify-content-center w-50 mx-auto align-items-center">
-                <div onClick={prevPage}>&#60;</div>
-                <ul className='d-flex justify-content-between align-items-center  m-0 w-50'>
-                    {
-                        arrayPages?.map(num=><li key={num}>{num}</li>)
-                    }
-                </ul>
-                <div>&#62;</div>
-            </div>
-        </section>
-    )
-}
-
-export default Pagination
+export default Pagination;
--- src/components/PokemonsList.jsx
+++ src/components/PokemonsList.jsx
@@ -1,34 +1,79 @@
-import React, { useEffect, useState } from "react";
-import Pagination from "./Pagination";
-import PokemonCard from "./PokemonCard";
+import React, { useEffect, useState } from 'react';
+import Pagination from './Pagination';
+import PokemonCard from './PokemonCard';
+import ReactPaginate from 'react-paginate';
 
 const PokemonsList = ({ urlsPokemons, pokemonByName }) => {
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [pageNumber, setPageNumber] = useState(0);
 
-  let pokemonsToShow = []
-  const pokemonsPerPage = 30;
+  let arrayPokemons = [];
+  const pokemonsPerPage = 20;
+  const lastPokemon = currentPage * pokemonsPerPage;
+  let pageCount = Math.ceil(urlsPokemons?.length / pokemonsPerPage);
 
-  if (urlsPokemons?.length < pokemonsPerPage) {
-    pokemonsToShow = [...urlsPokemons]
+  arrayPokemons = urlsPokemons?.slice(
+    lastPokemon - pokemonsPerPage,
+    lastPokemon
+  );
 
-  } else {
-    const lastPokemon = currentPage * pokemonsPerPage;
-    pokemonsToShow = urlsPokemons?.slice(lastPokemon - pokemonsPerPage, lastPokemon)
-  }
+  const displayUsers = urlsPokemons
+    ?.slice(lastPokemon, lastPokemon + pokemonsPerPage)
+    .map((url) => {
+      return <PokemonCard url={url} key={url} />;
+    });
 
-  let arrayPages = [];
-  let quantityPages = Math.ceil(urlsPokemons?.length / pokemonsPerPage);
-  const pagesPerBlock = 5;
-  let currentBlock = Math.ceil(currentPage, pagesPerBlock)
-  if (currentBlock * pagesPerBlock >= quantityPages) {
-    for (let i = currentBlock * pagesPerBlock - pagesPerBlock + 1; i <= quantityPages; i++) {
-      arrayPages.push(i)
-    }
-  } else {
-    for (let i = currentBlock * pagesPerBlock - pagesPerBlock + 1; i <= currentBlock * pagesPerBlock; i++) {
-      arrayPages.push(i)
-    }
-  }
+  const changePage = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
+  useEffect(() => {
+    pageCount = Math.ceil(urlsPokemons?.length / pokemonsPerPage);
+  }, [urlsPokemons]);
+
+  console.log(lastPokemon);
+  console.log(pokemonsPerPage);
+  console.log(displayUsers);
+
+  // displayUsers.map((url) => {
+  //   return <PokemonCard url={url} key={url} />;
+  // });
+
+  // if (urlsPokemons?.length < pokemonsPerPage) {
+  //   arrayPokemons = [...urlsPokemons];
+  // } else {
+  //   const lastPokemon = currentPage * pokemonsPerPage;
+  //   arrayPokemons = urlsPokemons.slice(
+  //     lastPokemon - pokemonsPerPage,
+  //     lastPokemon
+  //   );
+  // }
+
+  // let arrayPages = [];
+
+  // let quantityPages = Math.ceil(urlsPokemons.length / pokemonsPerPage);
+
+  // const pagesPerBlock = 5;
+  // let currentBlock = Math.ceil(currentPage / pagesPerBlock);
+  // if (currentBlock * pagesPerBlock >= quantityPages) {
+  //   for (
+  //     let i = currentBlock * pagesPerBlock - pagesPerBlock + 1;
+  //     i <= quantityPages;
+  //     i++
+  //   ) {
+  //     arrayPages.push(i);
+  //   }
+  // } else {
+  //   for (
+  //     let i = currentBlock * pagesPerBlock - pagesPerBlock + 1;
+  //     i <= currentBlock * pagesPerBlock;
+  //     i++
+  //   ) {
+  //     arrayPages.push(i);
+  //   }
+  // }
+  // console.log(arrayPokemons.length);
+  // console.log(arrayPages);
 
   return (
     <article className="row pokemon-list">
@@ -36,12 +81,22 @@ const PokemonsList = ({ urlsPokemons, pokemonByName }) => {
         <PokemonCard pokemonByName={pokemonByName} />
       ) : (
         <>
-          <Pagination arrayPages={arrayPages} currentPage={currentPage} setCurrentPage={setCurrentPage} quantityPages={quantityPages} />
-          {
-            pokemonsToShow?.map((url) => <PokemonCard url={url} key={url} />)
-          }
-        </>
+          {/* <Pagination arrayPages={arrayPages} /> */}
+          {/* {urlsPokemons?.map((url) => (
+            <PokemonCard url={url} key={url} />
+          ))} */}
 
+          <ReactPaginate
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={'paginate'}
+            previousLinkClassName={'previous-btn'}
+            nextLinkClassName={'next-btn'}
+            disabledClassName={'pagination-disabled'}
+            activeClassName={'pagination-active'}
+          />
+          {displayUsers}
+        </>
       )}
     </article>
   );
